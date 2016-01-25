8ad78d85a266031094df95fbc5c6be771f39bd9e
Ashish Waghmare <ashish.waghmare@talentica.com>
2016-01-26 01:05:41 +0530

    adding polymer starter kit to ui-module

--- src/main/java/in/co/waghmare/SwissReHackathonApplication.java
+++ src/main/java/in/co/waghmare/SwissReHackathonApplication.java
@@ -1,5 +1,6 @@
 package in.co.waghmare;
 
+import com.ibm.watson.developer_cloud.tradeoff_analytics.v1.TradeoffAnalytics;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +9,6 @@ public class SwissReHackathonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SwissReHackathonApplication.class, args);
+
 	}
 }
